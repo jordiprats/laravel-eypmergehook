25a8329ce34d4a4725f1c2893613950af098b7f0
Jordi Prats <jordi.prats@gmail.com>
2018-01-27 13:04:38 +0100

    simplificar

--- eypmergehook/app/Http/Controllers/MergeController.php
+++ eypmergehook/app/Http/Controllers/MergeController.php
@@ -15,16 +15,10 @@ class MergeController extends Controller
   public function mergeHook(Request $request)
   {
     # This hash signature is passed along with each request in the headers as X-Hub-Signature
-    if($request.hasHeader("X-Hub-Signature"))
-    {
-      $signature=header("X-Hub-Signature", string);
-      Log::info("signature: " + $signature);
-      Log::info($request->json()->all());
-      #signature = 'sha1=' + OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha1'), ENV['SECRET_TOKEN'], payload_body)
 
-      return [ 'penis'=> '8==D' ];
-    }
-    return [ 'penis'=> '8==D' ];
+    Log::info("signature: " + Request::header('X-Hub-Signature'););
+    Log::info($request->json()->all());
+    #signature = 'sha1=' + OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha1'), ENV['SECRET_TOKEN'], payload_body)
 
     return [ 'penis'=> '8==D' ];
   }
