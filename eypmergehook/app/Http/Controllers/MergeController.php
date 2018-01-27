5db6b7e9952f8d7e9c29a507a2ca5d56de67b325
Jordi Prats <jordi.prats@gmail.com>
2018-01-27 12:37:14 +0100

    firma github

--- eypmergehook/app/Http/Controllers/MergeController.php
+++ eypmergehook/app/Http/Controllers/MergeController.php
@@ -23,6 +23,8 @@ class MergeController extends Controller
         $signature=header("X-Hub-Signature", string);
         Log::info("signature: " + $signature);
         Log::info($request->json()->all());
+        #signature = 'sha1=' + OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha1'), ENV['SECRET_TOKEN'], payload_body)
+        
         return [ 'penis'=> '8==D' ];
       }
       return [ 'penis'=> '8==D' ];
