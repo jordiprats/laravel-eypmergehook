9fd357895a2e563cb6629f88298954673d1014f0
Jordi Prats <jordi.prats@gmail.com>
2018-01-27 18:35:42 +0100

    urls banejades

--- eypmergehook/app/Http/Middleware/VerifyCsrfToken.php
+++ eypmergehook/app/Http/Middleware/VerifyCsrfToken.php
@@ -10,6 +10,7 @@ class VerifyCsrfToken extends Middleware
      * The URIs that should be excluded from CSRF verification.
      */
     protected $except = [
-        '/github/webhook'
+        '/github/webhook',
+        '/github/webhook/*'
     ];
 }
