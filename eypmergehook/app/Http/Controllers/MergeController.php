532546880a80d44859991dbafb96749d2d6806af
Jordi Prats <jordi.prats@gmail.com>
2018-01-27 12:55:06 +0100

    isjoson no existeix -_-

--- eypmergehook/app/Http/Controllers/MergeController.php
+++ eypmergehook/app/Http/Controllers/MergeController.php
@@ -16,19 +16,16 @@ class MergeController extends Controller
   public function mergeHook(Request $request)
   {
     # This hash signature is passed along with each request in the headers as X-Hub-Signature
-    if($request.isJson())
+    if($request.hasHeader("X-Hub-Signature"))
     {
-      if($request.hasHeader("X-Hub-Signature"))
-      {
-        $signature=header("X-Hub-Signature", string);
-        Log::info("signature: " + $signature);
-        Log::info($request->json()->all());
-        #signature = 'sha1=' + OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha1'), ENV['SECRET_TOKEN'], payload_body)
-        
-        return [ 'penis'=> '8==D' ];
-      }
+      $signature=header("X-Hub-Signature", string);
+      Log::info("signature: " + $signature);
+      Log::info($request->json()->all());
+      #signature = 'sha1=' + OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha1'), ENV['SECRET_TOKEN'], payload_body)
+
       return [ 'penis'=> '8==D' ];
     }
+    return [ 'penis'=> '8==D' ];
 
     return [ 'penis'=> '8==D' ];
   }
