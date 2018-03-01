0415dd52387aae1a2ffc55e414caaea84a5b908a
Jordi Prats <jordi.prats@gmail.com>
2018-03-01 21:32:33 +0100

    api github

--- eypmergehook/app/User.php
+++ eypmergehook/app/User.php
@@ -42,4 +42,9 @@ class User extends Authenticatable
     return $this->hasMany(Organization::class);
   }
 
+  public function linkedsocialaccounts()
+  {
+    return $this->hasMany(LinkedSocialAccount::class);
+  }
+
 }
