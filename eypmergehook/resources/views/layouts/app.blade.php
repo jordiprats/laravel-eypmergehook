3f8f53302ce001e439fbbf43bba2f8b38226c92d
jordiprats <jordi.prats@gmail.com>
2018-03-03 22:29:41 +0100

    estil website

--- eypmergehook/resources/views/layouts/app.blade.php
+++ eypmergehook/resources/views/layouts/app.blade.php
@@ -47,11 +47,23 @@
                             <li><a href="{{ route('register') }}">Register</a></li>
                         @else
                             <li class="dropdown">
+
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
-                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                    <img src="https://github.com/{{ Auth::user()->nickname }}.png?size=40" /> <span class="caret"></span>
                                 </a>
 
                                 <ul class="dropdown-menu">
+                                    <li>
+                                        <a href="{{ route('user.edit') }}"
+                                            onclick="event.preventDefault();
+                                                     document.getElementById('settings-form').submit();">
+                                            Settings
+                                        </a>
+
+                                        <form id="settings-form" action="{{ route('user.edit') }}" method="GET" style="display: none;">
+                                        </form>
+                                    </li>
+                                    <li><hr /></li>
                                     <li>
                                         <a href="{{ route('logout') }}"
                                             onclick="event.preventDefault();
