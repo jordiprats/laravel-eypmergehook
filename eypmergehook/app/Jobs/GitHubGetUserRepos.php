2e88feecb8f4a9b0069adc2ad9c92ce7a3a0e433
Jordi Prats <jordi.prats@gmail.com>
2018-03-07 20:54:55 +0100

    creacio repos propis

--- eypmergehook/app/Jobs/GitHubGetUserRepos.php
+++ eypmergehook/app/Jobs/GitHubGetUserRepos.php
@@ -12,6 +12,7 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use GrahamCampbell\GitHub\Authenticators\AuthenticatorFactory;
 use GrahamCampbell\GitHub\GitHubFactory;
 use App\User;
+use App\Repo;
 use App\LinkedSocialAccount;
 
 
@@ -55,9 +56,41 @@ class GitHubGetUserRepos implements ShouldQueue
 
         $github_paginator  = new ResultPager($github);
 
-        foreach ($github_paginator->fetchAll($github->users(), 'repositories', [$user->nickname]) as $repo)
+        foreach ($github_paginator->fetchAll($github->users(), 'repositories', [$user->nickname]) as $github_repo)
         {
-          echo $repo['full_name']."\n";
+          echo $github_repo['full_name']."\n";
+
+          $repo = Repo::where(['clone_url' => $github_repo['clone_url']])->first();
+
+          // $table->string('repo_name');
+          // $table->string('full_name')->nullable();
+          // $table->string('project_name')->nullable();
+          // $table->boolean('fork');
+          // $table->boolean('private');
+          // $table->string('clone_url');
+          // $table->integer('user_id')->nullable()->references('id')->on('users');
+          // $table->integer('organization_id')->nullable()->references('id')->on('organizations');
+          // $table->boolean('telegram_notifications')->default(true);
+          // $table->string('telegram_chatid')->nullable();
+          if(!$repo)
+          {
+            if($github_repo['owner']['login']==$user->nickname)
+            {
+              $repo = Repo::create([
+                  'repo_name' => $github_repo['repo_name'],
+                  'full_name' => $github_repo['full_name'],
+                  'fork'      => $github_repo['fork'],
+                  'private'   => $github_repo['private'],
+                  'clone_url' => $github_repo['clone_url'],
+                  'user_id'   => $user->id,
+              ]);
+            }
+          }
+          else {
+            print_r($github_repo);
+          }
+
+
         }
         # $repos = $client->api('user')->repositories('KnpLabs');
         # $issue = $client->api('issue')->show('KnpLabs', 'php-github-api', 1);
