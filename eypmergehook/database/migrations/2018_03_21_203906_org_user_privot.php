ea0339aa3b31ffeff778c5280276b96fad9cd1b9
jordiprats <jordi.prats@gmail.com>
2018-03-21 21:43:50 +0100

    migracions creacio plataformes

---
+++ eypmergehook/database/migrations/2018_03_21_203906_org_user_privot.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class OrgUserPrivot extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('organization_user', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('organization_user');
+    }
+}
