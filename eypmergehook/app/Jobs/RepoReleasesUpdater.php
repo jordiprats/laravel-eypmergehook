6dc48ea27ecb8c2a4bf8f6e156f4448a23fe9322
Jordi Prats <jordi.prats@gmail.com>
2018-03-22 18:50:31 +0100

    substr

--- eypmergehook/app/Jobs/RepoReleasesUpdater.php
+++ eypmergehook/app/Jobs/RepoReleasesUpdater.php
@@ -125,7 +125,7 @@ class RepoReleasesUpdater implements ShouldQueue
             else
             {
               # verificar q no estigui en draft
-              $release = $github->repos()->releases()->show($this->owner, $this->repo, $github_tag['commit']['sha']);
+              $release = $github->repos()->releases()->show($this->owner, $this->repo, substr($github_tag['commit']['sha'], 0, 7));
 
               print_r($release);
             }
