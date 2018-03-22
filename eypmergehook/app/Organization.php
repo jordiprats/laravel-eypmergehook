8c5ab4f0ece571f9696c2cc4ec27aac3f7a486fd
Jordi Prats <jordi.prats@gmail.com>
2018-03-22 13:48:21 +0100

    org user relationship

--- eypmergehook/app/Organization.php
+++ eypmergehook/app/Organization.php
@@ -8,9 +8,15 @@ class Organization extends Model
 {
   // everything is mass assignable
   protected $guarded = [];
-  
+
   public function repos()
   {
     return $this->hasMany(Repo::class);
   }
+
+  public function users()
+  {
+      return $this->belongsToMany(User::class)->withTimestamps();;
+  }
+
 }
