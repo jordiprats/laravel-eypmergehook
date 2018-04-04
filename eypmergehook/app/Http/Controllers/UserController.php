0830af36ed3718f81ef6d8a33f5bd44350d58c72
jordiprats <jordi.prats@gmail.com>
2018-04-04 23:51:14 +0200

    typo

--- eypmergehook/app/Http/Controllers/UserController.php
+++ eypmergehook/app/Http/Controllers/UserController.php
@@ -89,7 +89,7 @@ class UserController extends Controller
 
   public function userPlatforms($user)
   {
-    return view('home')->with('user', User::where('username', $user)->first());
+    return view('home')->with('user', User::where('nickname', $user)->first());
   }
 
   /**
