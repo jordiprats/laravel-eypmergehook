d428f9904e520efcb65478d60f2bca0e357992b5
jordiprats <jordi.prats@gmail.com>
2018-04-09 23:31:57 +0200

    bugfix

--- eypmergehook/app/Http/Controllers/RepoReleaseController.php
+++ eypmergehook/app/Http/Controllers/RepoReleaseController.php
@@ -23,23 +23,15 @@ class RepoReleaseController extends Controller
     $user = User::where(['nickname' => $nickname])->first();
     $repo = Repo::where(['full_name' => $nickname."/".$repo, 'user_id' => $user->id])->first();
 
-    try
+    foreach ($github_paginator->fetchAll($github->repos()->releases(), 'all', [$nickname, $repo]) as $github_release)
     {
-      foreach ($github_paginator->fetchAll($github->repos()->releases(), 'all', [$nickname, $repo]) as $github_release)
+      if(!$repo->reporeleases->contains('release_name', $github_release['name']))
       {
-        if(!$repo->reporeleases->contains('release_name', $release['name']))
-        {
-          RepoRelease::create([
-            'release_name' => $release['name'],
-            'repo_id'      => $repo->id,
-          ]);
-        }
+        RepoRelease::create([
+          'release_name' => $github_release['name'],
+          'repo_id'      => $repo->id,
+        ]);
       }
-    }
-    catch (Exception $e)
-    {
-      //[2018-04-09 21:24:14] local.ERROR: Not Found {"exception":"[object] (Github\\Exception\\RuntimeException(code: 404): Not Found at /home/jprats/git/laravel-eypmergehook/eypmergehook/vendor/knplabs/github-api/lib/Github/HttpClient/Plugin/GithubExceptionThrower.php:87)
-      echo 'Caught exception: ',  $e->getMessage(), "\n";
     }
   }
 }
