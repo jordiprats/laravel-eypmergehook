1df247bc4b6a47458aa43116aa6159f04e26d2ad
jordiprats <jordi.prats@gmail.com>
2018-04-09 23:12:07 +0200

    debug info

--- eypmergehook/app/Http/Controllers/RepoReleaseController.php
+++ eypmergehook/app/Http/Controllers/RepoReleaseController.php
@@ -8,6 +8,7 @@ class RepoReleaseController extends Controller
 {
   public static function fetchGitHubRepoReleases($nickname, $repo, $github)
   {
+    Log::info("RepoReleaseController::fetchGitHubRepoReleases: ".$nickname."/".$repo);
     $github_paginator  = new ResultPager($github);
 
     foreach ($github_paginator->fetchAll($github->repos()->releases(), 'all', [$nickname, $repo]) as $github_release)
