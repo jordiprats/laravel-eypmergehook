00002ecadf8135b2d324c8b47d23b14eb7b0c8d0
jordiprats <jordi.prats@gmail.com>
2018-05-08 21:44:43 +0200

    tagging is blocking

--- eypmergehook/app/Jobs/Tagger.php
+++ eypmergehook/app/Jobs/Tagger.php
@@ -48,7 +48,7 @@ class Tagger implements ShouldQueue
     {
       $env="-e GITHUB_USERNAME=".$this->username." -e TELEGRAMTOKEN=".config('telegrameyptagger.TELEGRAMTOKEN')." -e TELEGRAMCHATID=".config('telegrameyptagger.TELEGRAMCHATID');
     }
-    $cmd="docker run -d -e ENABLE_DEBUG=1 ".$env." -v /root/.ssh:/root/.ssh -t eyp/eyptagger /bin/bash /usr/bin/updatetags.sh ".$this->repo;
+    $cmd="docker run -i -e ENABLE_DEBUG=1 ".$env." -v /root/.ssh:/root/.ssh -t eyp/eyptagger /bin/bash /usr/bin/updatetags.sh ".$this->repo;
     echo "tagging /".$cmd."/: ".exec($cmd)."\n";
   }
 }
